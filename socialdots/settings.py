09718daf71bfb49a91f731d2c601f5e47fcb7ce6
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-06-20 22:35:36 +0500

    whitenoise storage  added

--- socialdots/settings.py
+++ socialdots/settings.py
@@ -159,6 +159,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
