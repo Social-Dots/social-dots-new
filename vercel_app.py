38b8900986cd26382545fce82ab842532ff97c07
Dawood Ahmed <dawooda32100@gmail.com>
2025-09-16 21:07:51 +0500

    TEST: Absolute minimal WSGI app

--- vercel_app.py
+++ vercel_app.py
@@ -1,38 +1,8 @@
-# Ultra-minimal test WSGI app for debugging
+# Absolute minimal WSGI app to test Vercel deployment
 def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
+    return [b'<h1>Social Dots</h1><p>Basic WSGI working on Vercel!</p>']
 
-    # Try to import Django and show any errors
-    try:
-        import django
-        django_version = django.VERSION
-
-        import os
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialdots.settings')
-        django.setup()
-
-        from django.conf import settings
-        db_name = settings.DATABASES['default']['NAME']
-
-        content = f"""
-        <h1>Social Dots - Debug Test</h1>
-        <p>OK Python import works</p>
-        <p>OK Django version: {django_version}</p>
-        <p>OK Django setup successful</p>
-        <p>OK Database: {db_name}</p>
-        <p>If you see this, basic Django setup is working!</p>
-        """
-
-    except Exception as e:
-        content = f"""
-        <h1>Social Dots - Debug Test</h1>
-        <p>ERROR Error occurred: {str(e)}</p>
-        <p>Error type: {type(e).__name__}</p>
-        """
-
-    return [content.encode('utf-8')]
-
-# Vercel expects 'app'
 app = application
